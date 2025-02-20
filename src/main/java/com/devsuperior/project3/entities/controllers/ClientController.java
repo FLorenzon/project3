0d075de545323c6de7069ea01104317721242dc3
Felipe Lorenzon <srskru@gmail.com>
2025-02-20 16:57:05 -0300

    Database and seedings

---
+++ src/main/java/com/devsuperior/project3/entities/controllers/ClientController.java
@@ -0,0 +1,11 @@
+package com.devsuperior.project3.entities.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+	
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gepeto Duorovanko', '260.447.587-46', 8150.00, '1978-11-23', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Pietro Gatilho da Silva', '140.400.714-11', 11500.00, '1966-05-30', 6);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Mamoanaldo Polenta', '155.489.010-02', 4550.00, '2000-04-30', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Joaquim Escaramutchella', '280.845.177-75', 5780.00, '1994-01-20', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Pirórglio Jodingambucle', '195.157.266-14', 19250.00, '1961-12-15', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Angélica Vinagre Dutra', '454.478.911-53', 3800.00, '2002-05-01', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vandiogo Demétrio Marçal', '351.221.782-55', 19040.00, '1954-07-07', 6);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Olga Norma das Figueiras', '449.995.148-18', 8020.00, '2005-03-21', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Janete Pícollo Mendonça', '554.147.332-84', 13615.00, '1989-05-14', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Francisquinha Bolatcha Almeida', '144.465.667-71', 2540.00, '1964-12-09', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marla Éguas Bovinas', '492.691.998-82', 9250.00, '2000-03-04', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Patrícia Patrick Patronuss', '654.987.123-89', 28000.00, '1988-11-30', 4);
